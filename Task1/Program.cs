80f84c089e0a131a40838502a03359bc01c2deb5
Uliana Sokolova <uliana.sokolova.2000@gmail.com>
2024-07-16 21:43:44 +0300

    Добавленое задание1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////////Задача 1: Задайте значения M и N.
+///Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+///Использовать рекурсию, не использовать циклы.
+///
+void PrintNumbers(int M, int N)
+{
+if (M <= N)
+{
+Console.Write(M + " ");
+PrintNumbers(M + 1, N);
+}
+}
+
+Console.WriteLine("Задайте число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Натуральные числа в промежутке от {M} до {N}: ");
+PrintNumbers(M, N);
