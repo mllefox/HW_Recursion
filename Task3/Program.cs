8246793cc93aaaa597e651f8210a6daf50619a75
Uliana Sokolova <uliana.sokolova.2000@gmail.com>
2024-07-16 22:04:51 +0300

    Добавлена задача 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/////Задача 3: Задайте произвольный массив. 
+///Выведете его элементы, начиная с конца. 
+///Использовать рекурсию, не использовать циклы
+///
+
+void PrintArrayReverse(int[] array, int i)
+{
+    if (i >= 0)
+    {
+        Console.Write(array[i] + " ");
+        PrintArrayReverse(array, i - 1);
+    }
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[size];
+
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < size; i++)
+{
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Элементы массива в обратном порядке:");
+PrintArrayReverse(myArray, myArray.Length - 1);
